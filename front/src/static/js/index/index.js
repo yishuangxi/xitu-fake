781fc3c07f1ad74fbcfcd75bf0f3df72a67e9caf
yanshanshan <598811648@qq.com>
2016-08-25 23:28:39 +0800

    template render ok

--- front/src/static/js/index/index.js
+++ front/src/static/js/index/index.js
@@ -1,18 +1,14 @@
-
-
-$(function(){
+$(function () {
     var url = "http://localhost:8888/api/articles";
-    $.ajax(url,{
-        method:"GET",
-
-        async:true,
-
-        success:function(){
-            var data = data.data
-            console.log(data)
-
+    $.ajax(url, {
+        method: "GET",
+        async: true,
+        success: function (response) {
+            console.log(response)
+            var html = template('test', response)
+            $('.box-entries').append(html)
         },
-        error:function(){
+        error: function () {
 
         }
 
@@ -25,23 +21,23 @@ $(function(){
     var $li = $('.nav-list li')
     $li.click(function () {
         console.log(1112)
-       var $liIndex = $li.index(this)
+        var $liIndex = $li.index(this)
         console.log($liIndex)
-     $('.content').eq($liIndex).css("display","block").siblings().css("display","none")
+        $('.content').eq($liIndex).css("display", "block").siblings().css("display", "none")
     });
 
 
-   var $i =  $('i').filter('.action')
-    $i.hover(function(){
+    var $i = $('i').filter('.action')
+    $i.hover(function () {
         var $iIndex = $i.index(this)
         console.log($iIndex)
-        $i.next().eq($iIndex).css("display","block")
-    },function(){
+        $i.next().eq($iIndex).css("display", "block")
+    }, function () {
         var $iIndex = $i.index(this)
-        $i.next().eq($iIndex).css("display","none")
+        $i.next().eq($iIndex).css("display", "none")
     })
 
-    $('.hd-Login').click(function(){
+    $('.hd-Login').click(function () {
         console.log(11100)
     })
 
