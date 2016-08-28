e8f5cf6d4458617de51fd0c46da6428159b481c2
yanshanshan <598811648@qq.com>
2016-08-28 17:32:50 +0800

    头部点击下拉列表功能完成

--- front/src/static/js/user/user.js
+++ front/src/static/js/user/user.js
@@ -1,64 +1,10 @@
 /**
  * Created by db on 16/8/21.
  */
-// ;
-// (function ($) {
-//     $(function () {
-//         var url = '/api'+location.pathname;
-//
-//         var startTime = new Date().getTime()
-//         console.log('我马上要发请求了')
-//
-//        //$.ajax(url,{
-//        //    method:"GET",
-//        //    //data:params,
-//        //    //dataType:"json",
-//        //    async: true,
-//        //    success: function(){
-//        //        var $user = $('#user')
-//        //        var $entryTitle = document.createElement('div')
-//        //        var $img = document.createElement('img')
-//        //        $entryTitle.innerHTML = "为什么要使用 Node.js"
-//        //        $user.append($entryTitle)
-//        //        $img.attr=("src", "http://gold.xitu.io/images/welcome/entry-default-img@2x.png")
-//        //
-//        //    },
-//        //    error: function(){
-//        //
-//        //    }
-//        //});
-//
-//         $.ajax(url, {
-//             method: 'GET',
-//             async: true,
-//             success: function (data) {
-//
-//                 //console.log("请求成功, 耗时： ", endTime - startTime)
-//                 //console.log('data: ', data)
-//                 var data = data.data
-//                 var html = ''
-//
-//                 for(var p in data){
-//                     html =  html + '<div style="color: red">' +data[p] + '</div>'
-//                 }
-//
-//                 $('#user').append(html)
-//
-//
-//             },
-//             error: function (err) {
-//                 console.log(err)
-//             }
-//         })
-//
-//         console.log('我正在做其它事情。。。。。。。。')
-//
-//     })
-//
-// })(jQuery);
+
 
 ;(function(){
-    var url = '/api/user/100000'
+    var url = '/api/user/100000'    //用ajax请求数据渲染用户主页
     $.ajax(url,{
         method:"GET",
         async:"true",
@@ -75,4 +21,44 @@
 
         }
     })
+
+
+    //$('[cephalic="list"]').click(function(){   //头部头相点击弹出列表菜单
+    //    $('[cephalic="list-content"]').css('display','block')
+    //    console.log(111)
+    //
+    //},function(){
+    //    $('[cephalic="list-content"]').css('display','none')
+    //    console.log(222)
+    //})
+    //
+    $('[cephalic="list"]').click(function(event){
+        if($('[cephalic="list-content"]').css('display') === 'none'){
+            $('[cephalic="list-content"]').css('display','block')
+        } else{
+            $('[cephalic="list-content"]').css('display','none')
+        }
+
+        event.stopPropagation()
+    })
+
+    $('[ data-sel="body"]').click(function(){
+        if($('[cephalic="list-content"]').css('display') === 'block'){
+            $('[cephalic="list-content"]').css('display','none')
+        }else{
+            return
+        }
+
+    })
+
+    $('[data-sel="list2"]').hover(function(){
+        $('[data-sel="list2-content"]').css('display','block')
+
+    },function(){
+        $('[data-sel="list2-content"]').css('display','none')
+
+    })
+
+
+
 })();
