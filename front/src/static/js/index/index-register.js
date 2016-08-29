f8f6a329aa7b3357dd9dc92f4264fd18131d561b
ningfeijian <q_lele@qq.com>
2016-08-29 17:31:09 +0800

    t调试注册

--- front/src/static/js/index/index-register.js
+++ front/src/static/js/index/index-register.js
@@ -10,12 +10,22 @@
 //     })
 // })();
 
+//
+//
+;(function(){
+    $('[data-sel=hd-register]').click(function () {
+
+        $('[data-sel=i-want-register]').css("display","block")
+        console.log(123)
+
+    })
+})();
 
 
+;(function () {
+    $('[data-sel=register-Off]').click(function () {
 
-$('[data-sel=register]').click(function () {
-
-    $('[login=-index-register]').css("display","block")
-    console.log('aaa')
-
-})
+        $('[data-sel=i-want-register]').css("display","none")
+        console.log(987)
+    })
+})();
