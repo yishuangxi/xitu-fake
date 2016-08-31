1602a635d61070f67cbd821209e532b0fdd859be
yishuangxi <2546618112@qq.com>
2016-08-31 22:06:20 +0800

    后端模板渲染

--- page/base.py
+++ page/base.py
@@ -1,5 +1,9 @@
-#coding=utf8
+# coding=utf8
 from tornado.web import RequestHandler
+from service.user import ServiceUser
+
 
 class PageBase(RequestHandler):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(PageBase, self).__init__(*args, **kwargs)
+        self.srv_user = ServiceUser()
