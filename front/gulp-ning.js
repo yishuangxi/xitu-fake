d253261d4453f5c66bee94ef0c77add850fc2038
ningfeijian <q_lele@qq.com>
2016-09-05 22:21:20 +0800

    处理

--- front/gulp-ning.js
+++ front/gulp-ning.js
@@ -1,60 +1,60 @@
 /**
  * Created by db on 16/9/4.
-//  */
-// var gulp = require('gulp');
-//
-// gulp.task('ning', function(){
-//     gulp.src('./gulp-ning.js')
-//         .pipe(gulp.dest('dist'))
-// })
+ */
+var gulp = require('gulp');
+
+gulp.task('ning', function(){
+    gulp.src('./gulp.js')
+        .pipe(gulp.dest('dist'))
+})
 
 
-//
-//
-// var gulp = require('gulp');
-// //var fileinclude  = require('gulp-file-include');
-// var webserver = require('gulp-webserver');
-// //var htmlmin = require('gulp-htmlmin');
-//
-//
 
-gulp.task('ning-html', function(){
+
+var gulp = require('gulp');
+var fileinclude  = require('gulp-file-include');
+var webserver = require('gulp-webserver');
+var htmlmin = require('gulp-htmlmin');
+
+
+
+gulp.task('html', function(){
     gulp.src('src-ning/html/*.html')
-    //.pipe(fileinclude({
-    //    prefix: '@@',
-    //    basepath: '@file'
-    // }))
+    .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+    }))
         .pipe(gulp.dest('dist/html'))
 })
 
-//gulp.task('yan-css', function(){
-//    gulp.src('yan-src/static/css/**/*.css')
-//        .pipe(gulp.dest('dist/static/css'))
-//})
-//gulp.task('yan-js',function(){
-//    gulp.src('yan-src/static/js/**/*.js')
-//        .pipe(gulp.dest('dist/static/js'))
-//})
-//
-// //
-// gulp.task('ning-web', function() {
-//     gulp.src('dist/ning-html')
-//         .pipe(webserver({
-//             livereload: true,
-//             directoryListing: true,
-//             open: true
-//         }));
-// });
-
+gulp.task('css', function(){
+   gulp.src('src/static/css/**/*.css')
+       .pipe(gulp.dest('dist/static/css'))
+})
+gulp.task('js',function(){
+   gulp.src('src/static/js/**/*.js')
+       .pipe(gulp.dest('dist/static/js'))
+})
 
 //
-// gulp.task('watch', function(){
-//     gulp.watch(['src/html/*.html'], ['ning-html'])
-//     //gulp.watch(['yan-src/static/css/**/*.css'], ['yan-css'])
-//     //gulp.watch(['yan-src/static/js/**/*.js'], ['yan-js'])
-//     gulp.watch(['dist/ning-html'], ['ning-web'])
-// })
-//
-// gulp.task('build', ['ning-html','ning-web'])
-//
-// gulp.task('ning', ['build', 'watch'])
+gulp.task('web', function() {
+    gulp.src('dist/html')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: true,
+            open: true
+        }));
+});
+
+
+
+gulp.task('watch', function(){
+    gulp.watch(['src/html/*.html'], ['html'])
+    gulp.watch(['src/static/css/**/*.css'], ['css'])
+    gulp.watch(['src/static/js/**/*.js'], ['js'])
+    gulp.watch(['dist/html'], ['ning-web'])
+})
+
+gulp.task('build', ['html','web'])
+
+gulp.task('build', ['build', 'watch'])
