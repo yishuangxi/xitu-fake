d055966db46be49d1e931d8e4385119145b9f9f7
yanshanshan <598811648@qq.com>
2016-09-05 22:43:43 +0800

    test

--- front/gulp-yan.js
+++ front/gulp-yan.js
@@ -9,51 +9,69 @@
 //})
 
 
-
-var gulp = require('gulp');
+//
+//var gulp = require('gulp');
 //var fileinclude  = require('gulp-file-include');
-var webserver = require('gulp-webserver');
+//var webserver = require('gulp-webserver');
 //var htmlmin = require('gulp-htmlmin');
-
-
-
-gulp.task('yan-html', function(){
-    gulp.src('src-yan/html/*.html')
-        //.pipe(fileinclude({
-        //    prefix: '@@',
-        //    basepath: '@file'
-        //}))
-        .pipe(gulp.dest('dist/html'))
-})
-
+//
+//
+//
+//gulp.task('yan-html', function(){
+//    return gulp.src('src-yan/html/**/*.html')
+//        //.pipe(fileinclude({
+//        //    prefix: '@@',
+//        //    basepath: '@file'
+//        //}))
+//        .pipe(gulp.dest('dist/html'))
+//})
+//
 //gulp.task('yan-css', function(){
-//    gulp.src('yan-src/static/css/**/*.css')
+//    return gulp.src('yan-src/static/css/**/*.css')
 //        .pipe(gulp.dest('dist/static/css'))
 //})
 //gulp.task('yan-js',function(){
-//    gulp.src('yan-src/static/js/**/*.js')
+//    return gulp.src('yan-src/static/js/**/*.js')
 //        .pipe(gulp.dest('dist/static/js'))
 //})
 //
 //
-gulp.task('yan-web', function() {
-    gulp.src('./')
-        .pipe(webserver({
-            livereload: true,
-            directoryListing: true,
-            open: true
-        }));
-});
+////gulp.task('yan-web', function() {
+////    gulp.src('./')
+////        .pipe(webserver({
+////            livereload: true,
+////            directoryListing: true,
+////            open: true
+////        }));
+////});
+//
+//
+//
+//gulp.task('watch', function(){
+//    gulp.watch(['yan-src/html/**/*.html', 'yan-src/html/*.html'], ['yan-html'])
+//    gulp.watch(['yan-src/static/css/**/*.css'], ['yan-css'])
+//    gulp.watch(['yan-src/static/js/**/*.js'], ['yan-js'])
+//    //gulp.watch(['./'], ['yan-web'])
+//})
+//
+//gulp.task('build', ['yan-html','yan-css','yan-js'])
+//
+//gulp.task('yan', ['watch','build'])
 
 
+var gulp = require('gulp')
 
-gulp.task('watch', function(){
-    gulp.watch(['yan-src/html/*.html'], ['yan-html'])
-    //gulp.watch(['yan-src/static/css/**/*.css'], ['yan-css'])
-    //gulp.watch(['yan-src/static/js/**/*.js'], ['yan-js'])
-    gulp.watch(['./'], ['yan-web'])
+gulp.task('yan-html', function(){
+    return gulp.src('./src-yan/html/**/*.html')
+        .pipe(gulp.dest('./dist'))
 })
 
-gulp.task('build', ['yan-html','yan-web'])
+gulp.task('yan-watch', function(){
+    gulp.watch(['./src-yan/html/**/*.html'], ['yan-html'])
+})
 
-gulp.task('yan', ['watch','build'])
+gulp.task('yan-build', ['yan-watch', 'yan-html'])
+
+
+
+
