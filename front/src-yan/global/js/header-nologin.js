ec4fb8074d0a81db499ec7046ffa985e7de5f669
yanshanshan <598811648@qq.com>
2016-09-06 20:03:25 +0800

    登陆页引入/弹出成功

---
+++ front/src-yan/global/js/header-nologin.js
@@ -0,0 +1,9 @@
+/**
+ * Created by db on 16/9/6.
+ */
+;(function(){
+    $('[data-sel="login"]').click(function(){
+        console.log(打开登陆)
+        $('[data-sel="login-modal"]').css('display','block')
+    })
+})();
