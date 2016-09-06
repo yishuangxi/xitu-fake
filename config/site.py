22e0027b1b6fbe72d92dff8b0425c147d4d12fd5
yishuangxi <2546618112@qq.com>
2016-09-06 22:30:40 +0800

    shudown xsrf_cookies

--- config/site.py
+++ config/site.py
@@ -7,7 +7,7 @@ config_site = {
   'template_path': os.path.join(os.path.dirname(os.path.dirname(__file__)), 'front/dist'),
   'cookie_secret': '__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__',
   'login_url': '/login',
-  'xsrf_cookies': True,
+  # 'xsrf_cookies': True,
   'debug': True,
   'autoreload': True
 }
