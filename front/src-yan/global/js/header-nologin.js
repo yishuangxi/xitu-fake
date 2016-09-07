c13f459c9df10e9cb06c19d7b0d1bc21c565cfac
yanshanshan <598811648@qq.com>
2016-09-07 20:09:52 +0800

    未登陆首页完成

--- front/src-yan/global/js/header-nologin.js
+++ front/src-yan/global/js/header-nologin.js
@@ -2,8 +2,13 @@
  * Created by db on 16/9/6.
  */
 ;(function(){
-    $('[data-sel="login"]').click(function(){
+    $('[data-sel="login"]').click(function(){  //打开登陆
         console.log('打开登陆')
         $('[data-sel="login-modal"]').css('display','block')
+    })
+
+    $('[data-sel="hd-register"]').click(function(){
+       console.log('打开注册')
+        $('[data-sel="i-want-register"]').css('display','block')
     })
 })();
--- front/src-yan/global/js/login.js
+++ front/src-yan/global/js/login.js
@@ -5,20 +5,26 @@
     $('[ data-sel="shut-down"]').click(function(){    //关闭登陆弹框
         console.log('关闭登陆')
         $('[data-sel="login-modal"]').css('display','none')
+       $('[data-sel="username"]').val('')
+        $('[data-sel="password"]').val('')
+        $('.prompt').text('*请输入用户名及密码')
+
     })
 
-    $('[data-sel="login-button"]').click(function(){
+    $('[data-sel="login-button"]').click(function(){    //登陆－－把用户名与密码传
         var username = $('[data-sel="username"]').val()
         var password = $('[data-sel="password"]').val()
+
         $.post('/api/login',{
             username:username,
             password:password
         }).done(function(data){
             console.log(data)
             if(data.code === 1){
-                window.location.href ='/user/'+ data.data.id
+                //window.location.href ='/user/'+ data.data.id
             }else{
-                alert('您输入的用户名或密码错误，请重新输入！！')
+                //alert('您输入的用户名或密码错误，请重新输入！！')
+                $('.prompt').text('*您输入的用户名或密码错误！')
             }
 
 
