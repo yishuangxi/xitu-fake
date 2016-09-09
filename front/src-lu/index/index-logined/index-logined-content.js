ffa7d263349552154b26346f67787804872291e4
Rubby110 <1415897482@qq.com>
2016-09-09 19:24:13 +0800

    1.登录后头部js动作 2.publish页面html与css代码完成

--- front/src-lu/index/index-logined/index-logined-content.js
+++ front/src-lu/index/index-logined/index-logined-content.js
@@ -4,11 +4,15 @@
 
 
 ;(function(){
-    var url = '/api/articles'
+    var url = '/api/articles';
+
+    //ajax 请求 数据，动态生成页面index-main-content部分
     $.get(url,function(){
 
     }).done(function(data){
         var html = template('content-login',data);
         $('.home-entry-item').append(html)
-    })
+    });
+
 })();
+
