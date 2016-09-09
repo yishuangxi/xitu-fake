61086e12a7ff1e7305189aced6055daaba7d371c
yanshanshan <598811648@qq.com>
2016-09-09 20:48:45 +0800

    test

--- page/user.py
+++ page/user.py
@@ -16,6 +16,10 @@ class PageRegister(PageBase):
 class PageUser(PageBase):
   @coroutine
   def get(self, user_id):
-    # user = yield self.get_current_user()
-    user = yield self.srv_user.find_one_by_id(user_id)
-    self.render('user/user.html', user=user)
+
+    user = yield self.get_current_user()
+    if user:
+      user_info = user
+    else:
+      user_info = yield self.srv_user.find_one_by_id(user_id)
+    self.render('user/user.html', user_info=user_info, user=user)
